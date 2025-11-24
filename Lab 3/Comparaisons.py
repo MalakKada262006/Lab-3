76ccc4bff7edfdff3815b08073980e3520c1c33b
Malak El kadaoui <malak.elkadaoui93@gmail.com>
2025-11-24 23:52:24 +0100

    Add files via upload

---
+++ Lab 3/Comparaisons.py
@@ -0,0 +1,20 @@
+
+x = 20
+y = 15
+print(x == y)   
+print(x != y)   
+print(x < y)    
+print(x >= y)  
+print(x > y)    
+print(x <= y)  
+a1 = "abc"
+a2 = "abd"
+print(a1 < a2)   
+b1 = True
+b2 = False
+print(b1 == b2)  
+print(b1 != b2)
+a = 0.7 + 0.4
+b = 0.5
+print(a == b)                 
+print(abs(a - b) < 1e-9)
---
+++ Lab 3/lab3_operations.py
@@ -0,0 +1,10 @@
+
+a = 15
+b = 4
+print(f"{a} + {b} = {a + b}")
+print(f"{a} - {b} = {a - b}")
+print(f"{a} * {b} = {a * b}")
+print(f"{a} / {b} = {a / b}")      
+print(f"{a} // {b} = {a // b}")    
+print(f"{a} % {b} = {a % b}")     
+print(f"{a} ** {b} = {a ** b}")
